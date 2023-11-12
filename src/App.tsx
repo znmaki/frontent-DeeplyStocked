f78e33c356522eadbddccf6e5d3030208e1ff83b
znmaki <christhyan10@gmail.com>
2023-11-12 18:59:54 -0500

    archivo build para subir el deploy

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigator } from "./router"
-
-function App() {
-  return (
-    <Navigator />
-  )
-}
-
-export default App
--- src/modules/user/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './hooks/useLogin';
-export * from './hooks/useRegister';
-export * from './hooks/useUser';
-export { default as Authentication } from './pages/Authentication';
-export { default as ChangePassword } from './pages/ChangePassword';
-export { default as CreateUser } from './pages/CreateUser';
-export { default as LoginUser } from './pages/LoginUser';
-export { default as SendEmail } from './pages/SendEmail';
